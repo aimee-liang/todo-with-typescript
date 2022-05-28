9674ecff1688f5b32b577121adf33bdba987ad22
aimee-liang <aimeeeliang@gmail.com>
2022-05-27 21:01:22 -0400

    addNewItem() functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { v4 as uuidV4 } from 'uuid'
 
+type Task = {
+  id: string,
+  title: string,
+  completed: false,
+  createdAt: Date
+}
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("#task-input")
@@ -9,11 +15,19 @@ form?.addEventListener("submit", e => {
   e.preventDefault()
   if (input?.value == "" || input?.value == null) return
 
-  const task = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
-  input.value
+  
+  addNewItem(newTask)
 })
+
+const addNewItem = (task: Task) => {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+}
